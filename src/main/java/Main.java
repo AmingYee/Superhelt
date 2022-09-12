dd7dba6019a5ad61cb3acd68165906ffce61d301
AmingYee <wenm0007@stud.kea.dk>
2022-09-12 15:48:54 +0200

    all Maven stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String superHeroName;
-        String normalName;
-        boolean humanOrNot;
-        int creationYear;
-        int Strenght;
         Scanner sc = new Scanner(System.in);
-
-
+        System.out.println("Velkommen til superhelte database");
+        System.out.println("indtast superheltens navn");
+        String superHeroName = sc.next();
+        System.out.println("indtast virkiligt navn");
+        String virkeligtNavn = sc.next();
+        System.out.println("er han menneske eller ej");
+        String humanOrNot = sc.next();
+        System.out.println("indtast året han blev lavet");
+        int creationYear = sc.nextInt();
+        System.out.println("Indtast styrkeværdi");
+        int Strenght = sc.nextInt();
     }
 }
