b49d679d29068b321aaba48f397d95100eba71bd
AmingYee <wenm0007@stud.kea.dk>
2022-09-12 15:28:45 +0200

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String superHeroName;
+        String normalName;
+        boolean humanOrNot;
+        int creationYear;
+        int Strenght;
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
